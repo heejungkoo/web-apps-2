d816b959b95a334881b25a3abd7eda0e932ebf6d
Heejung Koo <heejung.koo@kellogg.northwestern.edu>
2025-02-20 23:55:44 +0000

    work on class 7

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,15 @@ Rails.application.routes.draw do
 
   resources "companies"
   # get("/companies", {:controller => "companies", :action => "index"})
+  get("/companies/:id", :controller => "companies", :action => "show")
+ 
+  # post("/companies", :controller => "companies", :action => "create")
+
   resources "contacts"
   # get("/contacts", {:controller => "contacts", :action => "index"})
+  # get("/contacts/new", {:controller => "contacts", :action => "new"})
+  # get("/contacts/:id", {:controller => "contacts", :action => "show"})
+  
 
   resources "posts"
   # get("/posts", {:controller => "posts", :action => "index"})
